3d6aac018dace1935b826d5651ed2aa7690c4603
Luis Iván Cuende <me@luisivan.net>
2016-01-16 19:33:03 +0100

    Back to the basics

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var email = document.getElementById('mail'),
+var email = document.getElementById('email'),
 	emailText = 'Please keep in mind that I have LOTS of emails.\nAnyway my email is me AT luisivan DOT net',
 	emailText = emailText.replace(' AT ', '@').replace(' DOT ', '.')
 
